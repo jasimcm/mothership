e0c3e13e291e65fb3239e677792eab02174b6a6f
abhinandnm <abhinandnm327@gmail.com>
2026-07-31 05:30:41 +0530

    Update SSH key to Windows PC key

--- user-vms/abhinandnm.nix
+++ user-vms/abhinandnm.nix
@@ -4,6 +4,6 @@
   tier = "large";
   enabled = true;
   keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB3OXIXYrp/+4m5vnp6ay1EsN0Qddl8eXo11FMe8QPut"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDBFPRE6eYwvknZPmEf3qCqzdXE5KbZDHKnSoL9sSglz realnm@BROOKHEAVEN"
   ];
 }
