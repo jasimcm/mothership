7d220df1f9ed54ee03ecb03b39a1984e07816da6
Muhammed Shibil M <muhammedshibilm321@gmail.com>
2026-08-03 09:53:19 +0530

    Change VM tier from 'large' to 'medium'

--- user-vms/shibil.nix
+++ user-vms/shibil.nix
@@ -1,6 +1,6 @@
 {
   github = "muhammedshibilm";
-  tier = "large";
+  tier = "medium";
   enabled = true;
   keys = [
        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII2RXsEXistFty449PjjsgAHekHiLfa8COv4R8hEwhla muhammedshibilm321@gmail.com"
