2d1687dac74ddb61c43fad20382e1240198ed20d
Godly K Mathews <97968185+godlykmathews@users.noreply.github.com>
2026-08-08 01:37:28 -0700

    Change VM tier from 'large' to 'small'

--- user-vms/godly.nix
+++ user-vms/godly.nix
@@ -1,6 +1,6 @@
 {
   github = "godlykmathews";
-  tier = "large";
+  tier = "small";
   enabled = true;
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOUPPVC8tGh8XDSFAOCR4jd1ePuqmztzXYLBju7n+l5 godlykmathews@outlook.com"
