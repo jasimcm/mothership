b3986f0ab0aefeb005dd152e9d51fe5a2966d29a
basith <basithofficialmail01@gmail.com>
2026-08-09 15:14:23 +0530

    change tier to small

--- user-vms/basith.nix
+++ user-vms/basith.nix
@@ -1,5 +1,5 @@
 {
-  tier = "medium";
+  tier = "small";
   enabled = true;
   keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGEdFVGYYna1Ng2BLoAxRENuND/CrxibCfZ+v1Opyzjc basithofficialmail01@gmail.com"
